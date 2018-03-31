0b6a2edeebe9cd401e48b6541484e3b708f92d9c
Cesar Jaldin <rodia.piedra@gmail.com>
2018-03-31 18:18:14 -0400

    define layout for admin

--- module/Admin/config/module.config.php
+++ module/Admin/config/module.config.php
@@ -9,17 +9,17 @@ use Zend\ServiceManager\Factory\InvokableFactory;
 return [
     'router' => [
         'routes' => [
-            'home' => [
+            'admin' => [
                 'type' => Literal::class,
                 'options' => [
-                    'route'    => '/',
+                    'route'    => '/admin',
                     'defaults' => [
                         'controller' => Controller\IndexController::class,
                         'action'     => 'index',
                     ],
                 ],
             ],
-            'application' => [
+            'segment' => [
                 'type'    => Segment::class,
                 'options' => [
                     'route'    => '/admin[/:action]',
@@ -43,10 +43,10 @@ return [
         'not_found_template'       => 'error/404',
         'exception_template'       => 'error/index',
         'template_map' => [
-            'layout/layout'     => __DIR__ . '/../view/layout/layout.phtml',
+            'admin/layout'      => __DIR__ . '/../view/layout/admin.phtml',
             'admin/index/index' => __DIR__ . '/../view/admin/index/index.phtml',
-            'error/404'         => __DIR__ . '/../view/error/404.phtml',
-            'error/index'       => __DIR__ . '/../view/error/index.phtml',
+//            'error/404'         => __DIR__ . '/../view/error/404.phtml',
+//            'error/index'       => __DIR__ . '/../view/error/index.phtml',
         ],
         'template_path_stack' => [
             __DIR__ . '/../view',
--- module/Admin/src/Controller/IndexController.php
+++ module/Admin/src/Controller/IndexController.php
@@ -1,9 +1,4 @@
 <?php
-/**
- * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
- * @copyright Copyright (c) 2005-2016 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- */
 
 namespace Admin\Controller;
 
@@ -14,6 +9,8 @@ class IndexController extends AbstractActionController
 {
     public function indexAction()
     {
+        $this->layout('admin/layout');
+
         return new ViewModel();
     }
 }
