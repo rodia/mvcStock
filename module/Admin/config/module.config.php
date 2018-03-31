f1e4844924cb642f32d2e42fa34d6d7b5ea5fc46
Cesar Jaldin <rodia.piedra@gmail.com>
2018-03-31 17:25:34 -0400

    add admin module

--- module/Admin/config/module.config.php
+++ module/Admin/config/module.config.php
@@ -1,9 +1,4 @@
 <?php
-/**
- * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
- * @copyright Copyright (c) 2005-2016 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- */
 
 namespace Admin;
 
@@ -27,7 +22,7 @@ return [
             'application' => [
                 'type'    => Segment::class,
                 'options' => [
-                    'route'    => '/application[/:action]',
+                    'route'    => '/admin[/:action]',
                     'defaults' => [
                         'controller' => Controller\IndexController::class,
                         'action'     => 'index',
@@ -48,10 +43,10 @@ return [
         'not_found_template'       => 'error/404',
         'exception_template'       => 'error/index',
         'template_map' => [
-            'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
-            'application/index/index' => __DIR__ . '/../view/application/index/index.phtml',
-            'error/404'               => __DIR__ . '/../view/error/404.phtml',
-            'error/index'             => __DIR__ . '/../view/error/index.phtml',
+            'layout/layout'     => __DIR__ . '/../view/layout/layout.phtml',
+            'admin/index/index' => __DIR__ . '/../view/admin/index/index.phtml',
+            'error/404'         => __DIR__ . '/../view/error/404.phtml',
+            'error/index'       => __DIR__ . '/../view/error/index.phtml',
         ],
         'template_path_stack' => [
             __DIR__ . '/../view',
